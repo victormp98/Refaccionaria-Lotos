d8b888bb25bbccc99753ed22ffcd5ff8455a6edb
Victor Manuel Perez <victor.manuel.pp98@gmail.com>
2026-02-01 17:42:51 -0600

    Aactualisasion y areglo del registro

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ using (var scope = app.Services.CreateScope())
     var context = services.GetRequiredService<ApplicationDbContext>();
 
     // AQUÍ ESTÁ EL TRUCO: Borra la basura y recrea todo
-    context.Database.EnsureCreated();
+    context.Database.Migrate();
 
     await DbInitializer.Initialize(services);
 }
